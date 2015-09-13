3704c14aa3f5d8b34bdd5432c5eae57169d29a8c
John Simon <john@johnsoft.com>
2015-09-13 12:48:32 -0400

    Add cross-browser mousewheel support

--- website/js/dragscroll.ts
+++ website/js/dragscroll.ts
@@ -1,63 +1,67 @@
 import { Point, Rectangle } from './coords';
+import { addWheelListener } from './mousewheel';
 
 class DragScroll {
-	private scrolling: boolean;
-	private lastX: number;
-	private lastY: number;
+    private scrolling: boolean;
+    private lastX: number;
+    private lastY: number;
 
-	public constructor(private callback: DragScroll.Callback) {}
+    public constructor(private callback: DragScroll.Callback) {}
 
-	public bindEvents(element: Element) {
-		element.addEventListener('mousedown', (e: MouseEvent) => this.mousedown(e));
-		element.addEventListener('mousewheel', (e: MouseWheelEvent) => this.mousewheel(e));
-		window.addEventListener('mousemove', (e: MouseEvent) => this.mousemove(e));
-		window.addEventListener('mouseup', (e: MouseEvent) => this.mouseup(e));
-	}
+    public bindEvents(element: Element) {
+        element.addEventListener('mousedown', (e: MouseEvent) => { this.mousedown(e); });
+        addWheelListener(element, e => { this.mousewheel(e); });
+        window.addEventListener('mousemove', (e: MouseEvent) => { this.mousemove(e); });
+        window.addEventListener('mouseup', (e: MouseEvent) => { this.mouseup(e); });
+    }
 
-	public mousedown(event: MouseEvent) {
-		this.scrolling = true;
-		this.lastX = event.pageX;
-		this.lastY = event.pageY;
-		event.preventDefault();
-	}
+    public mousedown(event: MouseEvent) {
+        this.scrolling = true;
+        this.lastX = event.pageX;
+        this.lastY = event.pageY;
+        event.preventDefault();
+    }
 
-	public mousemove(event: MouseEvent) {
-		if (!this.scrolling)
-			return;
-		this.callback.scrollRelative(this.lastX - event.pageX, this.lastY - event.pageY);
-		this.lastX = event.pageX;
-		this.lastY = event.pageY;
-	}
+    public mousemove(event: MouseEvent) {
+        if (!this.scrolling)
+            return;
+        this.callback.scrollRelative(this.lastX - event.pageX, this.lastY - event.pageY);
+        this.lastX = event.pageX;
+        this.lastY = event.pageY;
+    }
 
-	public mouseup(event: MouseEvent) {
-		this.scrolling = false;
-	}
+    public mouseup(event: MouseEvent) {
+        this.scrolling = false;
+    }
 
-	public mousewheel(event: MouseWheelEvent) {
-		if (this.scrolling)
-			return;
+    public mousewheel(event: WheelEvent) {
+        if (this.scrolling)
+            return;
 
-		var delta = event.wheelDelta;
-		var ratio = Math.pow(2, delta / -480);
-		var viewport = this.callback.getViewport();
-		var aroundPoint = new Point(viewport.left + viewport.width / 2, viewport.top + viewport.height / 2);
+        var lines = event.deltaMode === WheelEvent.DOM_DELTA_PIXEL ? event.deltaY / (100 / 3)
+            : event.deltaMode === WheelEvent.DOM_DELTA_LINE ? event.deltaY
+            : event.deltaY * 4;
+        var ratio = Math.pow(1.1, lines);
+        var viewport = this.callback.getViewport();
+        var aroundPoint = new Point(event.pageX, event.pageY)
+        var aroundPoint = new Point(viewport.left + viewport.width / 2, viewport.top + viewport.height / 2);
 
-		viewport.left -= aroundPoint.x;
-		viewport.top -= aroundPoint.y;
-		viewport = Rectangle.ltrb(viewport.left * ratio, viewport.top * ratio, viewport.right() * ratio, viewport.bottom() * ratio);
-		viewport.left += aroundPoint.x;
-		viewport.top += aroundPoint.y;
+        viewport.left -= aroundPoint.x;
+        viewport.top -= aroundPoint.y;
+        viewport = Rectangle.ltrb(viewport.left * ratio, viewport.top * ratio, viewport.right() * ratio, viewport.bottom() * ratio);
+        viewport.left += aroundPoint.x;
+        viewport.top += aroundPoint.y;
 
-		this.callback.setViewport(viewport);
-	}
+        this.callback.setViewport(viewport);
+    }
 }
 
 module DragScroll {
-	export interface Callback {
-		scrollRelative(x: number, y: number): void;
-		getViewport(): Rectangle;
-		setViewport(viewport: Rectangle): void;
-	}
+    export interface Callback {
+        scrollRelative(x: number, y: number): void;
+        getViewport(): Rectangle;
+        setViewport(viewport: Rectangle): void;
+    }
 }
 
 export { DragScroll as default };
