bc8d77bd6266ffd0da012029129dd705246f94f8
John Simon <john@johnsoft.com>
2015-09-15 11:11:11 -0400

    Use pm2 for deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,8 +13,5 @@ rsync -az $(dirname "$0")/../build/website/{static,views,*.js} "$REMOTE":/srv/du
 
 ssh "$REMOTE" <<\EOSH
     cd /srv/dustcourse
-    echo "************ PRESS CTRL+C ONCE THE SERVER STARTS LISTENING ******************"
-    echo "****************** IT'S OKAY, IT WILL KEEP LISTENING ************************"
-    killall node
-    PORT=80 nohup node index.js &
+    PORT=80 node_modules/.bin/pm2 restart index.js
 EOSH
