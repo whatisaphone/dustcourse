df5b884983c7e00dadf1dbe913fdc82a8097a783
John Simon <john@johnsoft.com>
2015-09-15 09:55:21 -0400

    Update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,16 +6,15 @@ set -o pipefail
 
 REMOTE="$1"
 
-echo "************ PRESS CTRL+C ONCE THE SERVER STARTS LISTENING ******************"
-echo "****************** IT'S OKAY, IT WILL KEEP LISTENING ************************"
-
 gulp build-website
 
 rsync -az $(dirname "$0")/../package.json "$REMOTE":/srv/dustcourse/
-rsync -az $(dirname "$0")/../build/website/{static,views,index.js} "$REMOTE":/srv/dustcourse/
+rsync -az $(dirname "$0")/../build/website/{static,views,*.js} "$REMOTE":/srv/dustcourse/
 
 ssh "$REMOTE" <<\EOSH
-    killall node
     cd /srv/dustcourse
+    echo "************ PRESS CTRL+C ONCE THE SERVER STARTS LISTENING ******************"
+    echo "****************** IT'S OKAY, IT WILL KEEP LISTENING ************************"
+    killall node
     PORT=80 nohup node index.js &
 EOSH
